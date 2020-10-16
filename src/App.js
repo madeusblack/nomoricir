0f0b7efe0417385e0421aa09fcd297b1f02e9be4
Yayson Olivares <yayson.olivares@gmail.com>
2020-10-15 22:15:07 -0300

    initial commit
    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Holi, No hay que moricir</h2>
+        <p>Aqui un video para que hagas la jajacion xd</p>
+        <iframe title="youtubeiframe" className="iframe" src="https://www.youtube.com/embed/XewbmK0kmpI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
