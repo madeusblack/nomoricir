550a0b92c989fcfd8cadf4a5683fcc25cc552f02
Yayson Olivares <yayson.olivares@gmail.com>
2020-10-16 00:16:11 -0300

    added subliminal messages
    added subliminal messages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h2>Holi, No hay que moricir</h2>
+        <h2 className="title">Holi, No hay que moricir</h2>
         <p>Aqui un video para que hagas la jajacion xd</p>
         <iframe title="youtubeiframe" className="iframe" src="https://www.youtube.com/embed/XewbmK0kmpI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <div className="subliminal">
+        <p >holi, soy un mensaje subliminal, eres genial y no te tienes que moricir</p>
+        <p >holi, soy otro mensaje subliminal</p>
+        <p >yo deberia estar durmiendo</p>
+        <p >y tu tambien</p>
+        <p >no importa la hora que leas esto, mimir hace bien</p>
+        </div>
         <a href="https://madeusblack.com/">Web del ser de luz que hizo esto por acá</a>
+
     </div>
   );
 }
